dcfd4b090b7db785c9a51977c96cf7eba6a04a32
Ic3man <11461271+deanjhutch@users.noreply.github.com>
2021-04-04 09:42:34 -0400

    Update PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -511,9 +511,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  14.35 // P14.35 I0.86 D59.55 from Autotune 
+    #define DEFAULT_Ki   0.86
+    #define DEFAULT_Kd  59.55
   #endif
 #endif // PIDTEMP
 
